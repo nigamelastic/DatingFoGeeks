61cc9248e9c581871a153f0a8a1132fae68df477
Kshitij Nigam <nigamelastic@gmail.com>
2019-06-07 15:49:50 +0200

    uploading the first working OKC Script
    contains code for adding cookies , recieving them and adding sessions

---
+++ okc.js
@@ -0,0 +1,92 @@
+
+/*
+*
+*
+*@name okc
+*
+*
+* @desc Basic session management for okc
+* 
+*
+*
+*
+*/
+
+const puppeteer = require('puppeteer');
+var fs = require('fs');
+var fileExists;
+
+const cookiesPath = 'jehrilicookies.txt';
+
+ 
+fs.exists(cookiesPath, function(exists) {
+    console.log("file exists ? " + exists);
+    fileExists=exists;
+});
+
+console.log('Starting');
+(async () => {
+
+    console.log('started');
+    // Due to the date format month should be -1 so for 1 april it should be 2019 ,3,1
+    var username = "usernam"
+    var password = "password"
+
+
+
+
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    if (fileExists) {
+        console.log('File exists');
+        
+            const content = fs.readFileSync(cookiesPath);
+            const cookiesArr = JSON.parse(content);
+            if (cookiesArr.length !== 0) {
+                for (let cookie of cookiesArr) {
+                    await page.setCookie(cookie)
+                }
+                console.log('Session has been loaded in the browser')
+            }
+        
+        await page.goto('https://www.okcupid.com/home', { "waitUntil": "networkidle0" });
+        await page.waitForSelector('#navigation > div.nav-left > ul > li:nth-child(1) > a')
+
+    } else {
+        console.log("file doesnt exist");
+        await page.goto('https://www.okcupid.com', { "waitUntil": "networkidle0" });
+        await page.click('#root > span > div > div > div.splashdtf-content > div.splashdtf-header > div.splashdtf-header-signin > a')
+        await page.waitForSelector('#root > span > div > div > div.login2017-container > span > div > form > div.login2017-fields > div:nth-child(1) > span.oknf-typable-wrapper.oknf-typable-wrapper--text > input');
+
+        await page.type('#root > span > div > div > div.login2017-container > span > div > form > div.login2017-fields > div:nth-child(1) > span.oknf-typable-wrapper.oknf-typable-wrapper--text > input', username);
+        await page.type('#root > span > div > div > div.login2017-container > span > div > form > div.login2017-fields > div:nth-child(2) > span.oknf-typable-wrapper.oknf-typable-wrapper--password > input', password)
+        await page.click('#root > span > div > div > div.login2017-container > span > div > form > div.login2017-actions > input')
+        await page.waitForSelector('#navigation > div.nav-left > ul > li:nth-child(1) > a')
+        
+        const cookiesObject = await page.cookies()
+        fs.writeFileSync(cookiesPath, JSON.stringify(cookiesObject));
+        console.log('Session has been saved to ' + cookiesPath);
+    }
+
+    
+    await page.waitForSelector('#match_results > span:nth-child(1) > div > a:nth-child('+1+') > div > div.usercard-thumb > span > img')
+    await page.click('#match_results > span:nth-child(1) > div > a:nth-child('+1+') > div > div.usercard-thumb > span > img')
+
+
+
+    await frame.waitFor(5000)
+    await browser.close()
+
+})()
+
+
+
+
+
+
+
+
+
+
+
