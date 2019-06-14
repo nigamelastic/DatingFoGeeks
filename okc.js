ba3fb11846387f06a679ebc42c35a73ad5998f61
Kshitij Nigam <nigamelastic@gmail.com>
2019-06-14 15:08:54 +0200

    updated okc.js with swipes

--- okc.js
+++ okc.js
@@ -15,6 +15,7 @@
 const puppeteer = require('puppeteer');
 var fs = require('fs');
 var fileExists;
+var step;
 
 const cookiesPath = 'jehrilicookies.txt';
 
@@ -50,12 +51,14 @@ console.log('Starting');
                 console.log('Session has been loaded in the browser')
             }
         
-        await page.goto('https://www.okcupid.com/home', { "waitUntil": "networkidle0" });
+        await page.goto('https://www.okcupid.com/home', { "waitUntil": "load" });
+        console.log("------------------------------------------------finishing the if statement")
         await page.waitForSelector('#navigation > div.nav-left > ul > li:nth-child(1) > a')
+        
 
     } else {
         console.log("file doesnt exist");
-        await page.goto('https://www.okcupid.com', { "waitUntil": "networkidle0" });
+        await page.goto('https://www.okcupid.com', { "waitUntil": "load" });
         await page.click('#root > span > div > div > div.splashdtf-content > div.splashdtf-header > div.splashdtf-header-signin > a')
         await page.waitForSelector('#root > span > div > div > div.login2017-container > span > div > form > div.login2017-fields > div:nth-child(1) > span.oknf-typable-wrapper.oknf-typable-wrapper--text > input');
 
@@ -63,19 +66,26 @@ console.log('Starting');
         await page.type('#root > span > div > div > div.login2017-container > span > div > form > div.login2017-fields > div:nth-child(2) > span.oknf-typable-wrapper.oknf-typable-wrapper--password > input', password)
         await page.click('#root > span > div > div > div.login2017-container > span > div > form > div.login2017-actions > input')
         await page.waitForSelector('#navigation > div.nav-left > ul > li:nth-child(1) > a')
-        
+        console.log("------------------------------------------------finishing the else statement")
         const cookiesObject = await page.cookies()
         fs.writeFileSync(cookiesPath, JSON.stringify(cookiesObject));
         console.log('Session has been saved to ' + cookiesPath);
     }
 
     
-    await page.waitForSelector('#match_results > span:nth-child(1) > div > a:nth-child('+1+') > div > div.usercard-thumb > span > img')
-    await page.click('#match_results > span:nth-child(1) > div > a:nth-child('+1+') > div > div.usercard-thumb > span > img')
+    await page.waitForSelector('#navigation > div.nav-left > ul > li:nth-child(2) > a')
+    await page.click('#navigation > div.nav-left > ul > li:nth-child(2) > a')
+    await page.waitForSelector('#quickmatch-wrapper > div > div > span > div > div.qm-content > div > div.qm-content-stackholder > span > div > div > div > div.qmcard-top > div.cardactions > button.cardactions-action.cardactions-action--like')
+    console.log('Starting the swipes ...........')
+    for (step = 0; step < 100; step++) {
+        await page.click('#quickmatch-wrapper > div > div > span > div > div.qm-content > div > div.qm-content-stackholder > span > div > div > div > div.qmcard-top > div.cardactions > button.cardactions-action.cardactions-action--like')
+        await page.waitFor(100)
+        console.log('swipe number .'+step)
+    }
+    
 
 
-
-    await frame.waitFor(5000)
+    await page.waitFor(5000)
     await browser.close()
 
 })()
